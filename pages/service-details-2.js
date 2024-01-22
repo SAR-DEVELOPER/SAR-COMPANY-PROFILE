852421212a4c2d16355b71cb779b41be3a6cb94d
SAR DEVELOPERm <sartm.developer@gmail.comm>
2024-01-22 15:31:42 +0700

    update service-details-2

--- pages/service-details-2.js
+++ pages/service-details-2.js
@@ -139,8 +139,9 @@ export default function ServiceDetails() {
                                     {/*-============spacing==========-*/}
                                 </div>
                                 <div className="position-relative br_left_3px_theme_color pd_left_10"> 
-                                Jasa ini merupakan jasa dalam menyelesaikan sengketa perpajakan yang timbul antara wajib pajak dengan otoritas pajak
-                                 </div>
+                                memberikan solusi komprehensif untuk kebutuhan penelitian Anda. Tim ahli kami siap membantu 
+                                dalam merancang, melaksanakan, dan menganalisis penelitian dengan cermat.                                 
+                                </div>
                                 {/*-============spacing==========-*/}
                                 <div className="pd_bottom_30" />
                                 {/*-============spacing==========-*/}
@@ -149,49 +150,71 @@ export default function ServiceDetails() {
                                         <div className={isActive.key == 5 ? "accordion  active-block" : "accordion"}>
                                             <div className={isActive.key == 5 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(5)}>
                                                 <div className="question_box ">
-                                                    <div className="title_no_a_18 trans">Pemeriksaan Pajak</div>
+                                                    <div className="title_no_a_18 trans">Kebijakan Akuntansi</div>
                                                     <span className="icon_fq trans fi-rs-arrow-small-right" />
                                                 </div>
                                             </div>
                                             <div className="answer accordion-content" style={{ display: `${isActive.key == 5 ? "block" : "none"}` }}> 
-                                            Pemeriksaan dimulai dari terbitnya Surat Perintah Pemeriksaan hingga terbitnya Surat Ketetapan Pajak (SKP).
+                                            Jasa riset Kebijakan Akuntansi menyediakan analisis mendalam terkait dengan standar dan metode akuntansi yang 
+                                            diterapkan dalam suatu organisasi.                                            
                                             </div>
                                         </div>
                                         <div className={isActive.key == 6 ? "accordion  active-block" : "accordion"}>
                                             <div className={isActive.key == 6 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(6)}>
                                                 <div className="question_box">
-                                                    <div className="title_no_a_18 trans">Keberatan</div>
+                                                    <div className="title_no_a_18 trans">Perhitungan Harga Pokok Produksi</div>
                                                     <span className="icon_fq trans fi-rs-arrow-small-right" />
                                                 </div>
                                             </div>
                                             <div className="answer accordion-content" style={{ display: `${isActive.key == 6 ? "block" : "none"}` }}> 
-                                            Pendampingan pengajuan keberatan berdasarkan Surat Ketetapan Pajak hingga diterbitkannya Surat Keputusan Keberatan.
-                                            </div>
+                                            Jasa riset Perhitungan Harga Pokok Produksi bertujuan untuk memberikan pemahaman mendalam tentang elemen biaya
+                                             yang terlibat dalam produksi suatu barang atau jasa.                                            
+                                             </div>
                                         </div>
                                         <div className={isActive.key == 7 ? "accordion  active-block" : "accordion"}>
                                             <div className={isActive.key == 7 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(7)}>
                                                 <div className="question_box ">
-                                                    <div className="title_no_a_18 trans">Banding / Gugatan</div>
+                                                    <div className="title_no_a_18 trans">Aksi Korporasi</div>
                                                     <span className="icon_fq trans fi-rs-arrow-small-right" />
                                                 </div>
                                             </div>
                                             <div className="answer accordion-content" style={{ display: `${isActive.key == 7 ? "block" : "none"}` }}>
-                                            Pendampingan penyelesaian sengketa pajak melalui jalur hukum pada 
-                                            Pengadilan Pajak dimulai dari Pengajuan Banding Berdasarkan Surat Keputusan 
-                                            Keberatan hingga diterbitkannya Putusan Banding/Gugatan oleh Pengadilan Pajak.
+                                            Jasa riset Aksi Korporasi fokus pada analisis peristiwa-peristiwa penting dalam perusahaan, 
+                                            seperti restrukturisasi, merger, akuisisi, atau penggabungan.
                                             </div>
                                         </div>
                                         <div className={isActive.key == 8 ? "accordion  active-block" : "accordion"}>
                                             <div className={isActive.key == 8 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(8)}>
                                                 <div className="question_box ">
-                                                    <div className="title_no_a_18 trans">Peninjauan Kembali</div>
+                                                    <div className="title_no_a_18 trans">Pengujian Kontrol Internal</div>
                                                     <span className="icon_fq trans fi-rs-arrow-small-right" />
                                                 </div>
                                             </div>
                                             <div className="answer accordion-content" style={{ display: `${isActive.key == 8 ? "block" : "none"}` }}>
-                                            Pendampingan penyelesaian sengketa pajak melalui jalur hukum luar biasa pada 
-                                            Mahkamah Agung dimulai dari Pengajuan Peninjauan Kembali Berdasarkan Putusan Pengadilan
-                                             Pajak hingga diterbitkannya Putusan Peninjauan Kembali oleh Mahkamah Agung.
+                                            Jasa riset Pengujian Kontrol Internal membantu organisasi dalam mengevaluasi dan meningkatkan efektivitas 
+                                            sistem kontrol internalnya.
+                                            </div>
+                                        </div>
+                                        <div className={isActive.key == 9 ? "accordion  active-block" : "accordion"}>
+                                            <div className={isActive.key == 9 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(9)}>
+                                                <div className="question_box ">
+                                                    <div className="title_no_a_18 trans">Analisis Standar Belanja (ASB)</div>
+                                                    <span className="icon_fq trans fi-rs-arrow-small-right" />
+                                                </div>
+                                            </div>
+                                            <div className="answer accordion-content" style={{ display: `${isActive.key == 9 ? "block" : "none"}` }}>
+                                            Jasa riset Analisis Standar Belanja (ASB) memberikan wawasan terhadap pengeluaran dan belanja perusahaan. 
+                                            </div>
+                                        </div>
+                                        <div className={isActive.key == 10 ? "accordion  active-block" : "accordion"}>
+                                            <div className={isActive.key == 10 ? "question faq_header active" : "question faq_header"} onClick={() => handleToggle(10)}>
+                                                <div className="question_box ">
+                                                    <div className="title_no_a_18 trans">Pengukuran Kinerja</div>
+                                                    <span className="icon_fq trans fi-rs-arrow-small-right" />
+                                                </div>s
+                                            </div>
+                                            <div className="answer accordion-content" style={{ display: `${isActive.key == 10 ? "block" : "none"}` }}>
+                                            Jasa riset Pengukuran Kinerja membantu organisasi dalam menilai sejauh mana pencapaian tujuan dan kinerja operasional. 
                                             </div>
                                         </div>
                                     </div>
