bae28f5653751c7778e03f5a7e75f876e81929eb
SAR DEVELOPERm <sartm.developer@gmail.comm>
2024-01-26 10:14:55 +0700

    eksperimen jasa dan layanan

--- components/layout/NavbarNav.js
+++ components/layout/NavbarNav.js
@@ -62,58 +62,56 @@ export default function NavbarNav() {
                     <Link href="/" className="nav_link">
                         <span className="text-link"> Jasa & Layanan </span>
                     </Link>
-                    <div className="row">
-                        <ul className="sub_menu">
-                            {/** Baris Satu */}
-                            <li className="menu-item nav-item vankine_menu_image">
-                                <Link href="/service-details-1#konsultasi-pajak" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-1.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Konsultasi Pajak </span>
-                                </Link>
-                            </li>
-                            <li className="menu-item vankine_menu_image">
-                                <Link href="/service-details-1#litigasi-sengketa-pajak" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-2.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Litigasi & Sengketa Pajak </span>
-                                </Link>
-                            </li>
-                            <li className="menu-item nav-item vankine_menu_image">
-                                <Link href="/service-details-1#merger-akuisisi" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-3.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Merger, Akuisisi dan Restrukturisasi Perusahaan </span>
-                                </Link>
-                            </li>
-                            <li className="menu-item nav-item vankine_menu_image">
-                                <Link href="/service-details-1#transfer-pricing" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-4.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Transfer Pricing </span>
-                                </Link>
-                            </li>
-                            <li className="menu-item  nav-item vankine_menu_image">
-                                <Link href="/home-5" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-5.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Jasa Pelatihan Akuntansi dan Perpajakan </span>
-                                </Link>
-                            </li>
-                            <li className="menu-item   nav-item vankine_menu_image">
-                                <Link href="/home-6" className="nav_link image_big_on_menu_two">
-                                    <span className="img-link">
-                                        <img src="/assets/images/service/service-6.png" alt="icon" className="menu_img" />
-                                    </span>
-                                    <span className="text-link text-center"> Jasa Riset </span>
-                                </Link>
-                            </li>
-                        </ul>
+                    <ul className="sub_menu">
+                        {/** Baris Satu */}
+                        <li className="menu-item nav-item vankine_menu_image">
+                            <Link href="/service-details-1#konsultasi-pajak" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-1.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Konsultasi Pajak </span>
+                            </Link>
+                        </li>
+                        <li className="menu-item vankine_menu_image">
+                            <Link href="/service-details-1#litigasi-sengketa-pajak" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-2.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Litigasi & Sengketa Pajak </span>
+                            </Link>
+                        </li>
+                        <li className="menu-item nav-item vankine_menu_image">
+                            <Link href="/service-details-1#merger-akuisisi" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-3.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Merger, Akuisisi dan Restrukturisasi Perusahaan </span>
+                            </Link>
+                        </li>
+                        <li className="menu-item nav-item vankine_menu_image">
+                            <Link href="/service-details-1#transfer-pricing" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-4.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Transfer Pricing </span>
+                            </Link>
+                        </li>
+                        <li className="menu-item  nav-item vankine_menu_image">
+                            <Link href="/home-5" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-5.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Jasa Pelatihan Akuntansi dan Perpajakan </span>
+                            </Link>
+                        </li>
+                        <li className="menu-item   nav-item vankine_menu_image">
+                            <Link href="/home-6" className="nav_link image_big_on_menu_two">
+                                <span className="img-link">
+                                    <img src="/assets/images/service/service-6.png" alt="icon" className="menu_img" />
+                                </span>
+                                <span className="text-link text-center"> Jasa Riset </span>
+                            </Link>
+                        </li>
                         <ul className="sub_menu">
                             {/* Baris dua */}
                             <li className="menu-item nav-item vankine_menu_image">
@@ -165,7 +163,7 @@ export default function NavbarNav() {
                                 </Link>
                             </li>
                         </ul>
-                    </div>
+                    </ul>
                     <div className="dropdown-btn"><span className="fa fa-angle-down" /></div>
                 </li>
                 <li className="menu-item  menu-item-has-children dropdown  mennucolumn_  nav-item">
