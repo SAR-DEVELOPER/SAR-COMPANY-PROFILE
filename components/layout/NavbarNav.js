ae3354b1fffa9a8abf14b9256afd4e86901efb89
SAR DEVELOPERm <sartm.developer@gmail.comm>
2024-01-26 10:42:46 +0700

    aa

--- components/layout/NavbarNav.js
+++ components/layout/NavbarNav.js
@@ -58,7 +58,7 @@ export default function NavbarNav() {
                     </ul>
                     <div className="dropdown-btn"><span className="fa fa-angle-down" /></div>
                 </li>
-                <li className="menu-item  menu-item-has-children dropdown  mennucolumn_two   nav-item">
+                <li className="menu-item  menu-item-has-children dropdown  mennucolumn_three   nav-item">
                     <Link href="/blog" className="nav_link">
                         <span className="text-link">
                             Blog
