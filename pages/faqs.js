0ead35002439d17677d12bcff7d247230ecdd596
SAR DEVELOPERm <sartm.developer@gmail.comm>
2024-01-31 10:17:14 +0700

    a

--- pages/faqs.js
+++ pages/faqs.js
@@ -47,7 +47,7 @@ export default function Faq() {
                                         Hubungi Kami <span> <i className=" fi-rr-arrow-small-up" /></span>
                                     </Link>
                                 </div>
-                                <div className="image_box_only  type_three">
+                                <div className="image_box_only type_two">
                                     <div className="left">
                                         <div className="image one">
                                             <img src=" assets/images/about/about-2-1.jpg" alt="img" className="img-fluid" />
