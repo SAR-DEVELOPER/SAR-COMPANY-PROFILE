eca029a91cc14ad81fef4913ee050320da085086
Wilson Nathanael <wilsonns1000@gmail.com>
2024-12-09 14:06:58 +0700

    temporary highlights and video

---
+++ components/sections/Solution_highlight.js
@@ -0,0 +1,11 @@
+export default function SolutionHighlight(){
+    return(
+        <div>
+            <div className="container solution_highlight_container">
+                <div className="solution_highlight_border">
+                    <video src="assets/videos/our_solution.mp4" autoPlay loop/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/solution.js
+++ pages/solution.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/layout/Layout";
 import HeroSimple from "@/components/sections/Herosimple";
+import SolutionHighlight from "@/components/sections/Solution_highlight";
 
 export default function Solution() {
     return (
         <Layout headerStyle={7} footerStyle={5}>
             <HeroSimple />
+            <SolutionHighlight />
         </Layout>
     )
 }
